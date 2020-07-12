42b4e91d6ea9aac39cb9f9fdeaea5983a38db0db
Maczi01 <mateusz.w.twardy@gmail.com>
2020-07-13 00:06:16 +0200

    Change funcional component to class

--- src/main/resources/static2/src/views/UserAssignmentView.js
+++ src/main/resources/static2/src/views/UserAssignmentView.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {Component, useContext, useEffect, useState} from 'react';
 import styled from 'styled-components';
 import background from "../assets/background.jpg"
 import AppContext from "../context/context";
@@ -20,38 +20,78 @@ const MainImage = styled.div`
   background-image: url(${background});
 `
 
-export const UserAssignmentView = ({match}) => {
-    const context = useContext(AppContext);
-    const selectedId = match.params.id;
-    const user = context.users.filter(i => i.id == selectedId)[0]
-    const [assignments, setAssignments ] = useState([])
-    // setAssignments(Assignment.getAssignment(selectedId))
-    //
+export class UserAssignmentView extends Component {
 
-    // Assignment.returnAsset(id)
-    //     .then(() => Users.getUsers())
-    //     .then(users => this.setState({users}))
+    state = {
+        assignments: [],
+        userId: this.props.match.params.id,
+    };
 
-    const fetchData = async () => {
-        const response = await Assignment.getAssignment(selectedId);
-        setAssignments(response)
+    componentDidMount() {
+        Assignment.getAssignment(this.state.userId)
+            .then(assignments => this.setState({assignments}))
+            .catch(error => console.log("Can not load data"));
     }
 
-    useEffect(() => {
+    returnAssignment = (id) => {
+        Assignment.returnAsset(id)
+            .then(() => Assignment.getAssignment(this.state.userId))
+            .then(assignments => this.setState({assignments}))
+    };
 
-        fetchData();
-    });
+    render() {
+        const selectedId = this.props.match.params.id;
 
-    console.log("lol");
-
-    return (
-        <ViewWrapper>
-            <MainImage/>
-            <AssignmentTable
-                user={user}
-                assignments={assignments}
-                returnAsset={context.returnAssignment}
-            />
-        </ViewWrapper>
-    )
+        return (
+            <AppContext.Consumer>
+                {context => (
+                    <ViewWrapper>
+                        <MainImage/>
+                        <AssignmentTable
+                            user={context.users.filter(i => i.id == selectedId)[0]}
+                            assignments={this.state.assignments}
+                            returnAsset={this.returnAssignment}
+                        />
+                    </ViewWrapper>
+                )}
+            </AppContext.Consumer>
+        )
+    }
 }
+
+// export const
+//     UserAssignmentView = ({match}) => {
+//         const context = useContext(AppContext);
+//         const selectedId = match.params.id;
+//         const user = context.users.filter(i => i.id == selectedId)[0]
+//         const [assignments, setAssignments] = useState([])
+//         // setAssignments(Assignment.getAssignment(selectedId))
+//         //
+//
+//         // Assignment.returnAsset(id)
+//         //     .then(() => Users.getUsers())
+//         //     .then(users => this.setState({users}))
+//
+//         const fetchData = async () => {
+//             const response = await Assignment.getAssignment(selectedId);
+//             setAssignments(response)
+//         }
+//
+//         useEffect(() => {
+//
+//             fetchData();
+//         });
+//
+//         console.log("lol");
+//
+//         return (
+//             <ViewWrapper>
+//                 <MainImage/>
+//                 <AssignmentTable
+//                     user={user}
+//                     assignments={assignments}
+//                     returnAsset={context.returnAssignment}
+//                 />
+//             </ViewWrapper>
+//         )
+//     }
