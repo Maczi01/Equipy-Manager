dc0b49bfd0997e0c1f360cf9dcf8e8ddcd50aab8
Maczi01 <mateusz.w.twardy@gmail.com>
2020-07-13 11:38:36 +0200

    changes equipy view to class component

--- src/main/resources/static2/src/views/AssignmentHistoryView.js
+++ src/main/resources/static2/src/views/AssignmentHistoryView.js
@@ -1,8 +1,8 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {Component, useContext, useEffect, useState} from 'react';
 import styled from 'styled-components';
 import background from "../assets/background.jpg"
 import AppContext from "../context/context";
-import {Equipy} from "../api/Api";
+import {Assignment, Equipy} from "../api/Api";
 import {HistoryTable} from "../components/HistoryTable";
 import {SearchBar} from "../components/SearchBar";
 import {UserTable2} from "../components/UserTable2";
@@ -21,60 +21,106 @@ const MainImage = styled.div`
   background-image: url(${background});
 `
 
-export const AssignmentHistoryView = ({match}) => {
-    const context = useContext(AppContext);
-    const selectedId = match.params.id;
-    const asset = context.assets.filter(i => i.id == selectedId)[0]
-    const [assignments, setAssignments] = useState();
-    // const assignments = context.assignments(asset.id)
-    console.log(asset.id)
-    console.log("created")
+export class AssignmentHistoryView extends Component {
 
-    useEffect(() => {
-        async function fetchData() {
-            const response = await Equipy.assignmentHistory(selectedId);
-            setAssignments(response);
-        }
-        fetchData();
-    }, []);
-    // const assignUser = async (id) => {
-    //     const assignment = {startData:  new Date(), endData: null, userId: id, assetId: asset.id}
-    //     const response = await Assignment.assignAssetToUser(assignment)
-    //     console.log(`Assignement: ${response}`)
-    // }
+    state = {
+        assignments: [],
+        assetId: this.props.match.params.id,
+    };
 
-    return (
-        <ViewWrapper>
-            <MainImage/>
-            <SearchBar
-                // catchText={catchText}
-            />
-            {context.users.length ?
-                <>
-                    <UserTable2
-                        assignUser={context.assignUser}
-                        asset={asset}
-                        users={context.users}
-                    />
+    componentDidMount() {
+        Equipy.assignmentHistory(this.state.assetId)
+            .then(assignments => this.setState({assignments}))
+            .catch(error => console.log("Can not load data"));
+    }
+
+    render() {
+        return (
+            <AppContext.Consumer>
+                {context => (
+                    <ViewWrapper>
+                        <MainImage/>
+                        <SearchBar
+                            // catchText={catchText}
+                        />
+                        <UserTable2
+                            assignUser={context.assignUser}
+                            asset={context.assets.filter(i => i.id == this.state.assetId)[0]}
+                            users={context.users}
+                        />
 
 
-                </>
-                : <p>nic</p>
-            }
-            {
-                context.assignments.length ?
-
-                    <>
                         <HistoryTable
-                            asset={asset}
-                            assignments={assignments}
+                            asset={context.assets.filter(i => i.id == this.state.assetId)[0]}
+                            assignments={this.state.assignments}
                             returnAsset={context.returnAssignment}
                         />
-                    </>
-                    :
-                    <p>No assignments history</p>
-            }
 
-        </ViewWrapper>
-    )
+                    </ViewWrapper>
+                )}
+            </AppContext.Consumer>
+
+        )
+    }
+
+
 }
+
+// export const AssignmentHistoryViews = ({match}) => {
+//     const context = useContext(AppContext);
+//     const selectedId = match.params.id;
+//     const asset = context.assets.filter(i => i.id == selectedId)[0]
+//     const [assignments, setAssignments] = useState();
+//     // const assignments = context.assignments(asset.id)
+//     console.log(asset.id)
+//     console.log("created")
+//
+//     useEffect(() => {
+//         async function fetchData() {
+//             const response = await Equipy.assignmentHistory(selectedId);
+//             setAssignments(response);
+//         }
+//
+//         fetchData();
+//     }, []);
+//     // const assignUser = async (id) => {
+//     //     const assignment = {startData:  new Date(), endData: null, userId: id, assetId: asset.id}
+//     //     const response = await Assignment.assignAssetToUser(assignment)
+//     //     console.log(`Assignement: ${response}`)
+//     // }
+//
+//     return (
+//         <ViewWrapper>
+//             <MainImage/>
+//             <SearchBar
+//                 // catchText={catchText}
+//             />
+//             {context.users.length ?
+//                 <>
+//                     <UserTable2
+//                         assignUser={context.assignUser}
+//                         asset={asset}
+//                         users={context.users}
+//                     />
+//
+//
+//                 </>
+//                 : <p>nic</p>
+//             }
+//             {
+//                 context.assignments.length ?
+//
+//                     <>
+//                         <HistoryTable
+//                             asset={asset}
+//                             assignments={assignments}
+//                             returnAsset={context.returnAssignment}
+//                         />
+//                     </>
+//                     :
+//                     <p>No assignments history</p>
+//             }
+//
+//         </ViewWrapper>
+//     )
+// }
